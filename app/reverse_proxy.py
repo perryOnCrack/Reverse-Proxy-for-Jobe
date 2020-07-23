17433bece85a43d30a6cf48c158c0adeb37b6c70
perryOnCrack <perry1998511@gmail.com>
2020-07-23 14:31:05 +0800

    Updated uWSGI configs and cache reading (lang) finished
    Updated uWSGI's configs to properly log logs.
    First part of cache return of request_language finished.

--- app/reverse_proxy.py
+++ app/reverse_proxy.py
@@ -1,20 +1,34 @@
 import os
+import sys
+import json
+import time
+from flask import Flask, jsonify
 
-from flask import Flask, json, jsonify
+# Some constants
+PATH_working_server = 'working_server.json'
+PATH_joeb_list = 'jobe_list.json'
+PATH_sorted_lang = 'sorted_lang.json'
+TBE = sys.maxsize # Time Before Expired
 
-
-from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/jobe/index.php/restapi/languages')
-def languages():
-    # Check working_server.json's mod time to determent if we need to generate new one
-    try:
-        with open('working_server.json', 'r'):
-            
 
-    return ret
+@app.route('/languages')
+def languages():
+    # Check working_server.json's mod time to determent 
+    # if we need to generate new one or using existing sorted_lang.json.
+    # If the file is not there, generate new one.
+    if os.path.exists(PATH_working_server) and ((time.time() - os.path.getmtime(PATH_working_server)) < TBE):
+        try:
+            with open(PATH_sorted_lang, 'r') as f:
+                return jsonify(json.loads(f.read()))
+        except:
+            print('ERROR: Failed reading "' + PATH_working_server + '"')
+    
+    # Generate new working_server.json.
+    # Return empty if any thing goes wrong from this point forward
+    # First we read in jobe_list.json
 
 if __name__ == '__main__':
     app.run()
