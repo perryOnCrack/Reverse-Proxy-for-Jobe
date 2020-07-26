49284d9cb4fb3d159479054d3225b6040a0cbfc2
perryOnCrack <perry1998511@gmail.com>
2020-07-26 23:08:52 +0800

    Update uWSGI Configs and Roadmaps

--- app/reverse_proxy.py
+++ app/reverse_proxy.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 import requests
 
 #======================================================
-# Some constants
+# Some parameters
 #======================================================
 # File path to resourses
 PATH_working_server = 'working_server.json'
@@ -16,13 +16,15 @@ PATH_sorted_lang = 'sorted_lang.json'
 TTL_working_server = 10 # working_server.json's expire time, in sec
 TTL_jobe_request = 3 # request timeout on every jobe server, in sec
 
+
 app = Flask(__name__)
+
 
 #======================================================
 # API call: get_languages
 #
 # Returns a json containing supporting languages
-# Somthing like this:
+# Something like this:
 # [["cpp", "7.4.0"], ["python3", "3.6.5"]]
 # 
 # Returns:
@@ -31,20 +33,19 @@ app = Flask(__name__)
 #   (but when will that ever happened?)
 #======================================================
 @app.route('/languages', methods = ['GET'])
-def languages():
-    # Check working_server.json's mod time to determent 
-    # if we need to generate new one or using existing sorted_lang.json.
-    # If the file is not there, generate new one.
-    if os.path.exists(PATH_working_server) and ((time.time() - os.path.getmtime(PATH_working_server)) < TTL_working_server):
+def get_languages():
+    # Check working_server.json and sorted_lang.json's modification time to determent if we need to generate new ones or using existing ones.
+    # If the either one of the files does not exsist, generate new ones.
+    if os.path.exists(PATH_working_server) and ((time.time() - os.path.getmtime(PATH_working_server)) < TTL_working_server) and \
+        os.path.exists(PATH_sorted_lang) and ((time.time() - os.path.getmtime(PATH_sorted_lang)) < TTL_working_server):
         print('INFO: Using existing sorted_lang.json...')
         try:
             with open(PATH_sorted_lang, 'r') as f:
                 return jsonify(json.loads(f.read())), 200
         except:
-            print('ERROR: Failed reading "' + PATH_working_server + '"')
+            print('ERROR: Failed reading "' + PATH_sorted_lang + '"')
     
     # Generate new working_server.json.
-    # Return empty if any thing goes wrong from this point forward
     # First we read in jobe_list.json
     print('INFO: Generating new working_server.json and sorted_lang.json...')
     jobe_list = ''
@@ -105,17 +106,19 @@ def languages():
 
     return jsonify(sorted_lang_list), 200
 
+
 #======================================================
 # API call: put_file
 #
 # Like the name sugguests, it put files in the server 
-# via PUT request.
+# via PUT request in parameter file_contents.
 # 
 # It save the file to this proxy instead of the actual 
 # jobe server until submit_run is called.
 #
 # It won't decode the base64 content sent to this proxy 
-# since the file content is non of its business.
+# since the file content is non of its business and we 
+# can save some cpu cycles by not doing it.
 # 
 # Returns:
 #  204 on success
@@ -124,35 +127,92 @@ def languages():
 #   provide a unique file ID
 #   - NOT impelemented atm.
 #======================================================
-@app.route('/files', methods=['PUT'])
-def put_file():
+@app.route('/files/<file_uid>', methods = ['PUT'])
+def put_file(file_uid):
     # First thing first, check parameter stucture.
 
     return '', 204
     return '', 400
 
+
+#======================================================
+# API call: check_file
+#
+# Like the name sugguests, it checks cached files in 
+# the server via HEAD request.
+# 
+# It checks if the file is present in this proxy 
+# instead of the actual jobe server.
+#
+# Returns:
+#  204 on success
+#  400 on illegal parameter
+#  404 on file not found
+#======================================================
+@app.route('/files/<file_uid>', methods = ['HEAD'])
+def check_file(file_uid):
+
+    return '', 204
+    return '', 400
+    return '', 404
+
+
+#======================================================
+# API call: submit_run
+#
+# The elephant in the room. It check if the file needed
+# is in this proxy, then decide which jobe server to
+# sned to and retrive and return result back to client.
+# 
+# Returns:
+# 200 when the runs is sucessful and with result
+# 202 when the run is pending on jobe
+#  - which is impossible because jobe server only runs
+#  in immediate mode right now
+# 400 on illegal parameter
+# 404 when file needed is not found on the proxy
+#======================================================
+@app.route('/runs', methods = ['POST'])
+def submit_runs(file_uid):
+    return '', 404
+
 #======================================================
 # API call: post_file
 #
 # Like the name sugguests, it put files in the server 
-# via POST request.
+# via POST request in parameter file_contents.
 # 
 # It save the file to this proxy instead of the actual 
 # jobe server until submit_run is called.
 #
-# It won't decode the base64 content sent to this
-# proxy since the file content is non of its business.
+# Hmmm, it isn't implemented by jobe yet. Returns 403
+# in current state.
 # 
 # Returns:
+# ====ONLY RESPONSES 403 IN CURRENT STATE====
 #  200 on success
 #  400 on contents are not a valid base-64 encoding
 #======================================================
-@app.route('/files', methods=['POST'])
+@app.route('/files', methods = ['POST'])
 def post_file():
-    # First thing first, check parameter stucture.
+    return '', 403
 
-    return '', 200
-    return '', 400
+
+#======================================================
+# API call: get_run_status
+#
+# Like the name sugguests, it gets status of a 
+# pending run.
+#
+# It's not being implemented by jobe right now.
+# Only Returns 404 in current state.
+# 
+# Returns:
+# ====ONLY RESPONSES 404 IN CURRENT STATE====
+#======================================================
+@app.route('/runresults/id', methods = ['GET'])
+def get_run_status():
+    return '', 404
 
 if __name__ == '__main__':
     app.run()
